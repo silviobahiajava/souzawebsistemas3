11cea4c540ea272c607f69fae8a412a24cb6ad07
souza matheus <souzamatheusfelipe1@gmail.com>
2022-05-01 12:05:56 -0300

    Criação da classe Cliente

---
+++ souzawebsistemas3/src/main/java/br/com/souzawebsistemas3/model/Cliente.java
@@ -0,0 +1,34 @@
+package br.com.souzawebsistemas3.model;
+
+public class Cliente {
+
+		private String nome;
+		private String rg;
+		private String cpf;
+		private String telefone;
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public String getRg() {
+			return rg;
+		}
+		public void setRg(String rg) {
+			this.rg = rg;
+		}
+		public String getCpf() {
+			return cpf;
+		}
+		public void setCpf(String cpf) {
+			this.cpf = cpf;
+		}
+		public String getTelefone() {
+			return telefone;
+		}
+		public void setTelefone(String telefone) {
+			this.telefone = telefone;
+		}
+		
+}
