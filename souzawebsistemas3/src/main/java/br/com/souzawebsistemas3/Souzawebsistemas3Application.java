e3942126e2d20cafd6d4619a89aeb81e76bf60e9
souza matheus <souzamatheusfelipe1@gmail.com>
2022-05-01 10:40:23 -0300

    Primeiro projeto para o git

---
+++ souzawebsistemas3/src/main/java/br/com/souzawebsistemas3/Souzawebsistemas3Application.java
@@ -0,0 +1,13 @@
+package br.com.souzawebsistemas3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Souzawebsistemas3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Souzawebsistemas3Application.class, args);
+	}
+
+}
